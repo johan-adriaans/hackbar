868047e8952af9a5b57e5a4adcdcd579da486a8f
johan.adriaans <johan.adriaans@2420866a-d570-279d-1ed7-5374903d2276>
2010-10-11 22:29:00 +0000

    Rebuilt complete function/object structure to class based setup within a HackBar namespace. Not stabe yet, needs better testing. I tried but im a bit drunk atm ;)

--- trunk/chrome/content/Toolbox.js
+++ trunk/chrome/content/Toolbox.js
@@ -1,4 +1,4 @@
-var hackBarToolbox = {
+HackBar.Toolbox = {
 
   alphabet: new Array( 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ),
